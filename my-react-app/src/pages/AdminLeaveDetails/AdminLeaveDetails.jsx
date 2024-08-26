442f0c47dbec7687e5d0bddd55ebb1cbfccadcce
Christian James Torres <xtian_5455@yahoo.com>
2024-08-26 23:22:20 +0800

    modified:   my-react-app/src/pages/AdminLeaveDetails/AdminLeaveDetails.jsx

--- my-react-app/src/pages/AdminLeaveDetails/AdminLeaveDetails.jsx
+++ my-react-app/src/pages/AdminLeaveDetails/AdminLeaveDetails.jsx
@@ -1,12 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Helmet } from 'react-helmet';
 import Sidebar from '../../Components/Sidebar';
+import SignatureCanvas from 'react-signature-canvas';
 
 function LeaveDetails() {
   const [submenuOpen, setSubmenuOpen] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedAction, setSelectedAction] = useState('');
   const [description, setDescription] = useState('');
+  const [disapprovalReasonB, setDisapprovalReasonB] = useState('');
+  const [disapprovalReasonD, setDisapprovalReasonD] = useState('');
+  const [currentDateTime, setCurrentDateTime] = useState(new Date());
+
+  const sigCanvasHR = useRef({});
+  const sigCanvasSupervisor = useRef({});
+  const sigCanvasManager = useRef({});
+  const sigCanvasGM = useRef({});
+
+  useEffect(() => {
+    const interval = setInterval(() => setCurrentDateTime(new Date()), 1000);
+    return () => clearInterval(interval);
+  }, []);
 
   const toggleSubmenu = () => {
     setSubmenuOpen(!submenuOpen);
@@ -23,9 +37,19 @@ function LeaveDetails() {
   };
 
   const handleAction = () => {
-    // Handle the selected action and description here
     console.log(`Action: ${selectedAction}, Description: ${description}`);
     closeModal();
+  };
+
+  const clearSignature = (sigCanvas) => sigCanvas.current.clear();
+
+  const saveSignature = (sigCanvas) => {
+    const signatureData = sigCanvas.current.getTrimmedCanvas().toDataURL('image/png');
+    console.log("Saved Signature Data:", signatureData);
+  };
+
+  const getCanvasWidth = () => {
+    return Math.min(window.innerWidth * 0.8, 800);
   };
 
   const leave = {
@@ -51,7 +75,7 @@ function LeaveDetails() {
       </Helmet>
 
       <div className="flex flex-col lg:flex-row min-h-screen">
-      <Sidebar/>
+        <Sidebar />
 
         {/* Main Content */}
         <main className="w-full lg:w-3/4 p-10 bg-gray-100">
@@ -119,47 +143,247 @@ function LeaveDetails() {
               </button>
             </div>
           </div>
+
+          {/* Details of Action UI */}
+          <div className="mt-12 bg-white p-8 shadow-lg rounded-md">
+          <h1 className="text-3xl font-bold mb-8">Details of Action</h1>
+            {/* Certification of Leave Credits */}
+            <div className="mb-8 p-6 border rounded-lg">
+              <h3 className="text-lg font-semibold mb-4">A. CERTIFICATION OF LEAVE CREDITS</h3>
+              <div className="mb-4">
+                <p className="mb-2 font-bold">
+                  As of {currentDateTime.toLocaleDateString()} {currentDateTime.toLocaleTimeString()}
+                </p>
+                <table className="w-full border border-gray-300">
+                  <thead>
+                    <tr className="bg-gray-200">
+                     <th className="border border-gray-300 px-4 py-2 text-left"> </th>
+                      <th className="border border-gray-300 px-4 py-2 text-left">Vacation Leave</th>
+                      <th className="border border-gray-300 px-4 py-2 text-left">Sick Leave</th>
+                      <th className="border border-gray-300 px-4 py-2 text-left">Mandatory/Forced Leave</th>
+                      <th className="border border-gray-300 px-4 py-2 text-left">Special Leave</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td className="border border-gray-300 px-4 py-2">Total Earned</td>
+                      <td className="border border-gray-300 px-4 py-2"></td>
+                      <td className="border border-gray-300 px-4 py-2"></td>
+                      <td className="border border-gray-300 px-4 py-2"></td>
+                      <td className="border border-gray-300 px-4 py-2"></td>
+                    </tr>
+                    <tr>
+                      <td className="border border-gray-300 px-4 py-2">Less this application</td>
+                      <td className="border border-gray-300 px-4 py-2"></td>
+                      <td className="border border-gray-300 px-4 py-2"></td>
+                      <td className="border border-gray-300 px-4 py-2"></td>
+                      <td className="border border-gray-300 px-4 py-2"></td>
+                    </tr>
+                    <tr>
+                      <td className="border border-gray-300 px-4 py-2">Balance</td>
+                      <td className="border border-gray-300 px-4 py-2"></td>
+                      <td className="border border-gray-300 px-4 py-2"></td>
+                      <td className="border border-gray-300 px-4 py-2"></td>
+                      <td className="border border-gray-300 px-4 py-2"></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+
+              {/* HR Officer Signature */}
+              <div className="mb-8">
+                <label className="block text-sm font-bold mb-2">Signature (HR Officer):</label>
+                <SignatureCanvas
+                  ref={sigCanvasHR}
+                  penColor="black"
+                  canvasProps={{ width: getCanvasWidth(), height: 200, className: 'border w-full p-2 rounded' }}
+                />
+                <div className="mt-2 flex space-x-4">
+                  <button
+                    type="button"
+                    onClick={() => clearSignature(sigCanvasHR)}
+                    className="bg-gray-500 text-white font-bold py-2 px-4 rounded"
+                  >
+                    Clear
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => saveSignature(sigCanvasHR)}
+                    className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+                  >
+                    Save
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            {/* Recommendation */}
+            <div className="mb-8 p-6 border rounded-lg">
+              <h3 className="text-lg font-semibold mb-4">B. RECOMMENDATION</h3>
+              <div className="mb-4">
+                <h4 className="font-semibold mb-2">For approval/disapproval of:</h4>
+                <textarea
+                  value={disapprovalReasonB}
+                  onChange={(e) => setDisapprovalReasonB(e.target.value)}
+                  className="border rounded w-full p-2 mt-2"
+                  placeholder="Add reason for disapproval here..."
+                  rows={3}
+                />
+                <button
+                  type="button"
+                  onClick={() => console.log("Note Saved: ", disapprovalReasonB)}
+                  className="bg-blue-500 text-white font-bold py-2 px-4 mt-2 rounded-md"
+                >
+                  Save Note
+                </button>
+              </div>
+
+              {/* Supervisor Signature */}
+              <div className="mb-8">
+                <label className="block text-sm font-bold mb-2">Signature (Immediate Supervisor):</label>
+                <SignatureCanvas
+                  ref={sigCanvasSupervisor}
+                  penColor="black"
+                  canvasProps={{ width: getCanvasWidth(), height: 200, className: 'border w-full p-2 rounded' }}
+                />
+                <div className="mt-2 flex space-x-4">
+                  <button
+                    type="button"
+                    onClick={() => clearSignature(sigCanvasSupervisor)}
+                    className="bg-gray-500 text-white font-bold py-2 px-4 rounded"
+                  >
+                    Clear
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => saveSignature(sigCanvasSupervisor)}
+                    className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+                  >
+                    Save
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            {/* Approval/Disapproval */}
+            <div className="mb-8 p-6 border rounded-lg">
+              <h3 className="text-lg font-semibold mb-4">C. APPROVED FOR:</h3>
+              <ul className="list-disc list-inside mb-4">
+                <li>1. CTC</li>
+                <li>2. Sick Leave (1 day with pay)</li>
+                <li>3. Date</li>
+              </ul>
+
+              <div className="mb-4">
+                <h4 className="font-semibold mb-2">Disapproved due to:</h4>
+                <textarea
+                value={disapprovalReasonD}
+                onChange={(e) => setDisapprovalReasonD(e.target.value)}
+                className="border rounded w-full p-2"
+                placeholder="Add reason for disapproval here..."
+                rows={3}
+              />
+              <button
+                type="button"
+                onClick={() => console.log("Note Saved: ", disapprovalReasonD)}
+                className="bg-blue-500 text-white font-bold py-2 px-4 mt-2 rounded-md"
+              >
+                Save Note
+              </button>
+              </div>
+
+              {/* Manager Signature */}
+              <div className="mb-8">
+                <label className="block text-sm font-bold mb-2">Signature (Manager):</label>
+                <SignatureCanvas
+                  ref={sigCanvasManager}
+                  penColor="black"
+                  canvasProps={{ width: getCanvasWidth(), height: 200, className: 'border w-full p-2 rounded' }}
+                />
+                <div className="mt-2 flex space-x-4">
+                  <button
+                    type="button"
+                    onClick={() => clearSignature(sigCanvasManager)}
+                    className="bg-gray-500 text-white font-bold py-2 px-4 rounded"
+                  >
+                    Clear
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => saveSignature(sigCanvasManager)}
+                    className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+                  >
+                    Save
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            {/* Final Approval/Disapproval */}
+            <div className="p-6 border rounded-lg">
+              <h3 className="text-lg font-semibold mb-4">D. FINAL APPROVAL/DISAPPROVAL:</h3>
+              <ul className="list-disc list-inside mb-4">
+                <li>Approved Leave of Absence</li>
+                <li>Disapproved due to insufficient leave credits</li>
+              </ul>
+
+              {/* GM Signature */}
+              <div className="mb-8">
+                <label className="block text-sm font-bold mb-2">Signature (General Manager):</label>
+                <SignatureCanvas
+                  ref={sigCanvasGM}
+                  penColor="black"
+                  canvasProps={{ width: getCanvasWidth(), height: 200, className: 'border w-full p-2 rounded' }}
+                />
+                <div className="mt-2 flex space-x-4">
+                  <button
+                    type="button"
+                    onClick={() => clearSignature(sigCanvasGM)}
+                    className="bg-gray-500 text-white font-bold py-2 px-4 rounded"
+                  >
+                    Clear
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => saveSignature(sigCanvasGM)}
+                    className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+                  >
+                    Save
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </main>
       </div>
 
-      {/* Modal */}
+      {/* Modal for setting action */}
       {isModalOpen && (
-        <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75">
-          <div className="bg-white p-8 rounded-md shadow-lg w-full max-w-md">
-            <h2 className="text-2xl font-semibold mb-4">Set Action</h2>
-            <div className="mb-4">
-              <label className="block text-lg font-semibold mb-2">Action:</label>
-              <select
-                className="w-full p-2 border border-gray-300 rounded-md"
-                value={selectedAction}
-                onChange={(e) => setSelectedAction(e.target.value)}
-              >
-                <option value="" disabled>Select Action</option>
-                <option value="Approve">Approve</option>
-                <option value="Decline">Decline</option>
-              </select>
-            </div>
-            <div className="mb-4">
-              <label className="block text-lg font-semibold mb-2">Description:</label>
-              <textarea
-                className="w-full p-2 border border-gray-300 rounded-md"
-                rows="4"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-              ></textarea>
-            </div>
-            <div className="flex justify-end">
-              <button
-                onClick={handleAction}
-                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-2"
-              >
-                Save
-              </button>
-              <button
-                onClick={closeModal}
-                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-              >
+        <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex justify-center items-center">
+          <div className="bg-white p-8 rounded shadow-lg max-w-sm mx-auto">
+            <h2 className="text-xl font-semibold mb-4">Set Action</h2>
+            <select
+              className="w-full p-2 border rounded mb-4"
+              value={selectedAction}
+              onChange={(e) => setSelectedAction(e.target.value)}
+            >
+              <option value="">Select Action</option>
+              <option value="approved">Approve</option>
+              <option value="disapproved">Disapprove</option>
+            </select>
+            <textarea
+              className="w-full p-2 border rounded mb-4"
+              rows="3"
+              placeholder="Description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            <div className="flex justify-end space-x-4">
+              <button onClick={closeModal} className="bg-gray-500 text-white py-2 px-4 rounded">
                 Cancel
+              </button>
+              <button onClick={handleAction} className="bg-blue-500 text-white py-2 px-4 rounded">
+                Submit
               </button>
             </div>
           </div>
